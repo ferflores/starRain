18605570632b8648c812612a7b75aa4156ffd31e
Fernando Flores <ferflores@gmail.com>
2015-11-01 22:41:45 -0600

    gulp

--- server/index.js
+++ server/index.js
@@ -1,20 +1,19 @@
-var express = require("express");
+var express = require('express');
 var app = express();
-var bodyParser = require("body-parser");
+var bodyParser = require('body-parser');
+var path = require('path');
 
-
+app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, '..', 'client/views'));
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
 
-var router = express.Router();
-
-router.get('/', function(req, res){
-	res.json({message: 'ok'});
-});
-
-app.use('/api', router);
+app.get('/', function (req, res) {
+  res.render('index');
+})
 
 app.listen(port);
-console.log("Listening on port: " + port);
+console.log("Listening on port: " + port + " :)");
+
