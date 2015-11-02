a55d4bd3024e046ba68ba9be5ff7b27f20def06d
Fernando Flores <ferflores@gmail.com>
2015-11-01 21:33:54 -0600

    first commit

---
+++ server/index.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+var router = express.Router();
+
+router.get('/', function(req, res){
+	res.json({message: 'ok'});
+});
+
+app.use('/api', router);
+
+app.listen(port);
+console.log("Listening on port: " + port);
