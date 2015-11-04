f601bdda658bb1438bc63e69db8c8c32a873a159
Fernando Flores <ferflores@gmail.com>
2015-11-04 08:21:56 -0600

    compress js

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,10 @@
 var gulp = require('gulp');
-
-gulp.task('default', function(){
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+ 
+gulp.task('compress', function() {
+  return gulp.src('client/js/**/*.js')
+    .pipe(uglify())
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('public/js'));
 });
