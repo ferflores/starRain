b2f26cfe82c4beaf0e02ba48c71802a9590a179d
Fernando Flores <ferflores@gmail.com>
2015-11-04 10:50:05 -0600

    start scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var nodemon = require('gulp-nodemon');
  
 gulp.task('compress', function() {
   return gulp.src('client/js/**/*.js')
@@ -8,3 +9,12 @@ gulp.task('compress', function() {
     .pipe(concat('app.js'))
     .pipe(gulp.dest('public/js'));
 });
+
+gulp.task('start', function () {
+  nodemon({
+    script: 'server/app.js'
+  , env: { 'NODE_ENV': 'development' }
+  })
+})
+
+gulp.task('default',['compress', 'start']);
